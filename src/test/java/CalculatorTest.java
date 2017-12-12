1ca1ac111457d8b2b3589f7186cbf972091ad38f
ZydrunasG <zydrunas.gaidukovas@gmail.com>
2017-12-12 13:53:09 +0200

    calculator test

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,46 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class CalculatorTest {
+
+    private  Calculator calculator;
+    @Before
+    public void setUp() throws Exception
+    {
+       calculator = new Calculator(10,5);
+    }
+
+
+    @Test
+    public void testCreateCalculatorObject()
+    {
+        Assert.assertNotNull(calculator);
+    }
+
+    @Test
+    public void testCalculateSum()
+    {
+        Assert.assertEquals(15, calculator.calculateSum(), 0);
+    }
+
+    @Test
+    public void testCalculateSubtract()
+    {
+        Assert.assertEquals(5, calculator.calculateSubtract(), 0);
+    }
+
+    @Test
+    public void testCalculateMultiply()
+    {
+        Assert.assertEquals(150,calculator.calculateMultiply(), 0);
+    }
+
+    @Test
+    public void testCalculateDivision()
+    {
+        Assert.assertEquals(2, calculator.calculateDivision(), 0);
+    }
+
+
+}
